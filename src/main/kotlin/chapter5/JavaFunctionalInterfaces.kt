c2281ac19495471e1820dabcdd38acdbcc232647
MetalTurtle <dekolis1218@gmail.com>
2021-07-08 17:24:12 -0700

    Undo IntelliJ formatting

--- src/main/kotlin/chapter5/JavaFunctionalInterfaces.kt
+++ src/main/kotlin/chapter5/JavaFunctionalInterfaces.kt
@@ -2,6 +2,5 @@ package chapter5
 
 fun main() {
     // You can pass lambdas as functional interfaces to Java methods and the compiles will figure out the intended type
-    val runnable =
-        Runnable { println("Hello") } // This lambda is explicitly compiled to the Runnable functional interface type
+    val runnable = Runnable { println("Hello") } // This lambda is explicitly compiled to the Runnable functional interface type
 }
