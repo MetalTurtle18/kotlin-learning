85c864fbcbfe1890ab40637124759f39df313594
MetalTurtle <dekolis1218@gmail.com>
2021-07-08 17:23:09 -0700

    Add chapter 5 part 4 (5.4) file and README

---
+++ src/main/kotlin/chapter5/JavaFunctionalInterfaces.kt
@@ -0,0 +1,7 @@
+package chapter5
+
+fun main() {
+    // You can pass lambdas as functional interfaces to Java methods and the compiles will figure out the intended type
+    val runnable =
+        Runnable { println("Hello") } // This lambda is explicitly compiled to the Runnable functional interface type
+}
